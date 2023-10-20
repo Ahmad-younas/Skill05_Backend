85d71dd91d87e8b0bf8f5dcf2c5c32a5d1080f0d
Ahmad-younas <ahmadyounas2k18@gmail.com>
2023-10-20 12:59:30 +0500

    error solve

--- src/server.js
+++ src/server.js
@@ -12,9 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("../assets"));
 app.use(express.static("../assets/files"));
+app.use(express.static("../assets/build"));
 // app.use("files", express.static(__dirname + "/assets/files"));
 console.log("path",__dirname + '/build');
-app.use(express.static(path.join(__dirname+ "/build")));
+// app.use(express.static(path.join(__dirname + "/build")));
 app.use('/health',(req,res)=>{
     res.send("OK from server").status(200);
 })
